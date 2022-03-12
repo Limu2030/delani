bdd1662d017cbe278b681223aa12718f590031ca
limu2030 <limu2030@gmail.com>
2022-03-12 20:25:00 +0300

    adds hover on the what we do section using js

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 // list the icons
-var img = document.querySelectorAll(".js-image");
+var img = document.querySelectorAll(".js-image",".js-image2",".js-image3");
+// for every icon add an event listener
+img.forEach((el) => {
+  // get the text container for each icon. i.e. the clicked icon
+  var nextDisplay = el.nextElementSibling;
+  // when the icon is clicked hide the text container
+  el.addEventListener("click", () => {
+    // this is where we display the text container
+    nextDisplay.style.display = "flex";
+    // this is where we hide the icon
+    el.style.display = "none";
+  });
+
+  // from here the reverse is happening
+  // when the text container is clicked, the icon is displayed and the container hidden
+  nextDisplay.addEventListener("click", () => {
+    nextDisplay.style.display = "none";
+    el.style.display = "flex";
+  });
+});
+
+// <!-- 2 -->
+
+// list the icons
+var img = document.querySelectorAll(".js-image2");
+// for every icon add an event lietener
+img.forEach((el) => {
+  // get the text container for each icon. i.e. the clicked icon
+  var nextDisplay = el.nextElementSibling;
+  // when the icon is clicked hide the text container
+  el.addEventListener("click", () => {
+    // this is where we display the text container
+    nextDisplay.style.display = "flex";
+    // this is where we hide the icon
+    el.style.display = "none";
+  });
+
+  // from here the reverse is happening
+  // when the text container is clicked, the icon is displayed and the container hidden
+  nextDisplay.addEventListener("click", () => {
+    nextDisplay.style.display = "none";
+    el.style.display = "flex";
+  });
+});
+
+// <!-- 3 -->
+
+// list the icons
+var img = document.querySelectorAll(".js-image3");
 // for every icon add an event lietener
 img.forEach((el) => {
   // get the text container for each icon. i.e. the clicked icon
@@ -79,80 +127,11 @@ $(".bigPort-8").mouseout(function () {
 
 // feedback section
 
-$("#feedback").submit(function(){
-  var name = this.NAME.value
-  var email = this.EMAIL.vaue
-  var message = this.MESSAGE.value
-  alert(name + " we have received your message. Thank you for reaching out to us")
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // <!-- 2 -->
-
-// // list the icons
-// var img = document.querySelectorAll(".js-image");
-// // for every icon add an event lietener
-// img.forEach((el) => {
-//   // get the text container for each icon. i.e. the clicked icon
-//   var nextDisplay = el.nextElementSibling;
-//   // when the icon is clicked hide the text container
-//   el.addEventListener("click", () => {
-//     // this is where we display the text container
-//     nextDisplay.style.display = "flex";
-//     // this is where we hide the icon
-//     el.style.display = "none";
-//   });
-
-//   // from here the reverse is happening
-//   // when the text container is clicked, the icon is displayed and the container hidden
-//   nextDisplay.addEventListener("click", () => {
-//     nextDisplay.style.display = "none";
-//     el.style.display = "flex";
-//   });
-// });
-
-// // <!-- 3 -->
-
-// // list the icons
-// var img = document.querySelectorAll(".js-image");
-// // for every icon add an event lietener
-// img.forEach((el) => {
-//   // get the text container for each icon. i.e. the clicked icon
-//   var nextDisplay = el.nextElementSibling;
-//   // when the icon is clicked hide the text container
-//   el.addEventListener("click", () => {
-//     // this is where we display the text container
-//     nextDisplay.style.display = "flex";
-//     // this is where we hide the icon
-//     el.style.display = "none";
-//   });
-
-//   // from here the reverse is happening
-//   // when the text container is clicked, the icon is displayed and the container hidden
-//   nextDisplay.addEventListener("click", () => {
-//     nextDisplay.style.display = "none";
-//     el.style.display = "flex";
-//   });
-// });
+$("#feedback").submit(function () {
+  var name = this.NAME.value;
+  var email = this.EMAIL.vaue;
+  var message = this.MESSAGE.value;
+  alert(
+    name + " we have received your message. Thank you for reaching out to us"
+  );
+});
